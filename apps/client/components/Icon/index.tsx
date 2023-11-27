ab61352438f51f0d67f44e702d085c8258789eb6
juancho120 <juan07123@gmail.com>
2023-11-27 15:44:53 -0300

    client/add icons

---
+++ apps/client/components/Icon/index.tsx
@@ -0,0 +1,8 @@
+export { AlertIcon } from "./Components/AlertIcon";
+export { MenuIcon } from "./Components/MenuIcon";
+export { LocationIcon } from "./Components/LocationIcon";
+export { DropIcon } from "./Components/DropIcon";
+export { MeasurerIcon } from "./Components/MeasurerIcon";
+export { RulerIcon } from "./Components/RulerIcon";
+export { ThermometerIcon } from "./Components/ThermometerIcon";
+export { CompasIcon } from "./Components/CompasIcon";
---
+++ apps/client/components/Icon/type.tsx
@@ -0,0 +1,4 @@
+export type IconProps = {
+  size?: 20 | 24;
+  color?: string;
+};
