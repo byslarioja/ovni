9646d6be05e4ec0591eec335df6f1f5e969384a8
Matias Santillan <sntlln.93@gmail.com>
2024-01-10 19:44:21 -0300

    add typeorm config

---
+++ apps/api/README.md
@@ -0,0 +1,7 @@
+# Awesome Project Build with TypeORM
+
+Steps to run this project:
+
+1. Run `yarn i` command
+2. Setup database settings inside `config/database.ts` file
+3. Run `yarn start` command
---
+++ apps/api/src/config/database.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "ovni",
+  password: "password",
+  database: "ovni",
+  synchronize: false,
+  logging: false,
+  entities: ["src/entities/*.ts"],
+  migrations: ["src/database/migrations/*.ts"],
+});
