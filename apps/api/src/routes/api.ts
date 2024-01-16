341419658d7072fa5d5ec98061747805699d5e58
Matias Santillan <sntlln.93@gmail.com>
2024-01-16 12:57:47 -0300

    api/auth middleware

--- apps/api/src/routes/api.ts
+++ apps/api/src/routes/api.ts
@@ -3,8 +3,6 @@ import authRouter from "@app/http/controllers/auth/auth.routes";
 
 const router = Router();
 
-router.get("/", (req, res) => res.json({ message: "test" }));
-
 router.use("/auth", authRouter);
 
 export default router;
