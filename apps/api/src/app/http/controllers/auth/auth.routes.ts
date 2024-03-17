aca6b9979822ea316ec826b01905d149ab91fc78
Matias Santillan <sntlln.93@gmail.com>
2024-03-17 00:39:56 -0300

    api/forgot password

--- apps/api/src/app/http/controllers/auth/auth.routes.ts
+++ apps/api/src/app/http/controllers/auth/auth.routes.ts
@@ -4,10 +4,18 @@ import { validate } from "@app/http/middlewares/validator";
 import { RegistrableUserSchema } from "./register/schema";
 import { LoginUserSchema } from "./login/schema";
 import { login } from "./login";
+import { ForgotPasswordSchema } from "./forgot-password/schema";
+import { forgotPassword } from "./forgot-password";
+import { auth } from "@app/http/middlewares/auth";
 
 const authRouter = Router();
 
 authRouter.post("/register", validate(RegistrableUserSchema), register);
 authRouter.post("/login", validate(LoginUserSchema), login);
+authRouter.post(
+  "/forgot-password",
+  validate(ForgotPasswordSchema),
+  forgotPassword
+);
 
 export default authRouter;
