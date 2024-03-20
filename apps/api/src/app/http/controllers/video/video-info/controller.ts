8a18365903a5ce830b9c67202040b0e9bf00264e
Matias Santillan <sntlln.93@gmail.com>
2024-03-19 21:35:20 -0300

    api/fix newvideoinfo storing error

--- apps/api/src/app/http/controllers/video/video-info/controller.ts
+++ apps/api/src/app/http/controllers/video/video-info/controller.ts
@@ -14,6 +14,7 @@ export const videoInfoController = async (
 
     return res.status(201).json(videoInfo);
   } catch (error) {
+    console.error(error);
     return res.status(500).send({ message: "Something went wrong" });
   }
 };
--- apps/api/src/app/repositories/video.repository.ts
+++ apps/api/src/app/repositories/video.repository.ts
@@ -1,16 +1,19 @@
 import { VideoInfo } from "@app/entities/VideoInfo";
+import { VideoInfoRequest } from "@app/http/controllers/video/video-info/schema";
 import { AppDataSource } from "@config/database";
 
-export const createVideoInfo = async (newVideoInfo) => {
+export const createVideoInfo = async (
+  newVideoInfo: VideoInfoRequest["body"]
+) => {
   const videoInfo = new VideoInfo();
   const videoInfoRepository = AppDataSource.getRepository(VideoInfo);
 
   videoInfo.hash = newVideoInfo.hash;
-  videoInfo.id_from_video = newVideoInfo.id_from_video;
-  videoInfo.device_uri = newVideoInfo.device_uri;
-  videoInfo.width = newVideoInfo.width;
-  videoInfo.height = newVideoInfo.height;
-  videoInfo.duration = newVideoInfo.duration;
+  videoInfo.id_from_video = newVideoInfo.asset.id;
+  videoInfo.device_uri = newVideoInfo.asset.uri;
+  videoInfo.width = newVideoInfo.asset.width;
+  videoInfo.height = newVideoInfo.asset.height;
+  videoInfo.duration = newVideoInfo.asset.duration;
 
   const craetedVideoInfo = await videoInfoRepository.save(videoInfo);
 
