553d848e6f22caa10a1f908851a8059d5ab36615
Matias Santillan <sntlln.93@gmail.com>
2024-03-27 03:40:32 -0300

    client/add error boundary

--- apps/client/app/camera/_layout.tsx
+++ apps/client/app/camera/_layout.tsx
@@ -1,7 +1,11 @@
+import { ErrorImage } from "Components/ErrorImage";
 import { useOrientation } from "Shared/hooks/useOrientation";
-import { Slot } from "expo-router";
+import Theme from "Shared/theme";
+import { translate } from "Shared/utils/translate";
+import { ErrorBoundaryProps, Slot } from "expo-router";
 import { OrientationLock } from "expo-screen-orientation";
 import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function AuthLayout() {
   const { lockOrientation } = useOrientation();
@@ -15,3 +19,58 @@ export default function AuthLayout() {
     </>
   );
 }
+
+export function ErrorBoundary(props: ErrorBoundaryProps) {
+  console.log(props.error);
+  const lang = translate({
+    en: {
+      TITLE: "Something went wrong",
+      RETRY_BUTTON: "Retry?",
+    },
+    es: {
+      TITLE: "Algo salió mal",
+      RETRY_BUTTON: "¿Intentar de nuevo?",
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <View style={{ marginBottom: 30 }}>
+        <ErrorImage />
+      </View>
+      <Text style={styles.title}>{lang.t("TITLE")}</Text>
+      <Text style={styles.subtitle}>{props.error.message}</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>{lang.t("RETRY_BUTTON")}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Theme.color.neutral.background,
+  },
+  title: {
+    color: "#FFF",
+    fontSize: 24,
+    marginBottom: 10,
+  },
+  subtitle: {
+    color: "#888",
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#FFF",
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "#000",
+    fontSize: 16,
+  },
+});
