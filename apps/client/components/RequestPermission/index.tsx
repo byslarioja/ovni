9a6dd2ca460c4279a95ff68edfe92e8ba0f50fd1
Matias Santillan <sntlln.93@gmail.com>
2024-03-28 00:22:17 -0300

    🚧 client/style request permission component WIP

--- apps/client/components/RequestPermission/index.tsx
+++ apps/client/components/RequestPermission/index.tsx
@@ -1,30 +1,50 @@
-import { Button, StyleSheet, View, Text } from "react-native";
-import Constants from "expo-constants";
-import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text } from "react-native";
+import Theme from "Shared/theme";
+import { Button } from "Components/Button";
 
 export default function RequestPermission({
   title,
+  description,
   requestPermission,
 }: RequestPermission) {
+  const onPermissionRequest = () => {
+    requestPermission();
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={{ textAlign: "center" }}>{title}</Text>
-      <Button onPress={requestPermission} title="grant permission" />
-      <StatusBar style="dark" />
+      <Text style={styles.title}>{title}</Text>
+      {description && <Text style={styles.subtitle}>{description}</Text>}
+      <Button onPress={onPermissionRequest} text="grant permission" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: Constants.statusBarHeight,
-    display: "flex",
-    flexDirection: "column",
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "10%",
+    gap: 20,
+    backgroundColor: Theme.color.neutral.background,
+  },
+  title: {
+    textAlign: "center",
+    color: "#FFF",
+    fontSize: 24,
+    marginBottom: 10,
+  },
+  subtitle: {
+    textAlign: "center",
+    color: "#888",
+    fontSize: 18,
+    marginBottom: 20,
   },
 });
 
 type RequestPermission = {
   title: string;
+  description?: string;
   requestPermission: () => void;
 };
