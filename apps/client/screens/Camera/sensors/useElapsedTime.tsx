a39f0d221b35bab5ec8f82673fef92c7d070a6e9
Matias Santillan <sntlln.93@gmail.com>
2024-04-09 00:28:21 -0300

    client/ TODO, elapsedTimeAtom <- startTimeAtom

--- apps/client/screens/Camera/sensors/useElapsedTime.tsx
+++ apps/client/screens/Camera/sensors/useElapsedTime.tsx
@@ -1,6 +1,9 @@
 import { atom } from "jotai";
 import { PrimitiveTimeAtom, TimeReading } from "./types";
 
+/**
+ * TODO: elapsedTimeAtom should depend on startTimeAtom
+ */
 export const elapsedTimeAtom = atom("00:00:00");
 export const startTimeAtom = atom<TimeReading>(null) as PrimitiveTimeAtom;
 export const endTimeAtom = atom<TimeReading>(null) as PrimitiveTimeAtom;
