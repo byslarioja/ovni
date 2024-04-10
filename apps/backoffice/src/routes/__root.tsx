2f8fb215e9034794f9bd5c1c6868513a2ac30e0e
Matias Santillan <sntlln.93@gmail.com>
2024-04-10 20:11:25 -0300

    backoffice/exclude router dev tools in production

--- apps/backoffice/src/routes/__root.tsx
+++ apps/backoffice/src/routes/__root.tsx
@@ -3,8 +3,8 @@ import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import "../globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React, { Suspense } from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 const queryClient = new QueryClient();
 
@@ -19,8 +19,22 @@ export const Route = createRootRoute({
           </div>
         </div>
         <ReactQueryDevtools initialIsOpen={false} />
-        <TanStackRouterDevtools />
+        <Suspense>
+          <TanStackRouterDevtoolsProduction />
+        </Suspense>
       </QueryClientProvider>
     </TooltipProvider>
   ),
 });
+
+const TanStackRouterDevtoolsProduction =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        }))
+      );
