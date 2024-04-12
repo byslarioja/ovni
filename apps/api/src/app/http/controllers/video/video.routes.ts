9ea9172270789e86adb30d4b11853be06373508a
Matias Santillan <sntlln.93@gmail.com>
2024-04-12 03:08:49 -0300

    api/install multer and test file upload

--- apps/api/src/app/http/controllers/video/video.routes.ts
+++ apps/api/src/app/http/controllers/video/video.routes.ts
@@ -4,10 +4,28 @@ import { validate } from "@app/http/middlewares/validator";
 import { auth } from "@app/http/middlewares/auth";
 import { VideoSchema, createVideoController } from "./create-video";
 import { getVideosController } from "./get-videos";
+import multer from "multer";
+
+// Set up multer for file uploads
+const upload = multer({
+  dest: "uploads/",
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(mp4)$/)) {
+      // Only allow .mp4 files
+      return cb(new Error("Please upload an MP4 video"));
+    }
+
+    cb(null, true);
+  },
+});
 
 const videoRouter = Router();
 
-videoRouter.post("/", [auth, validate(VideoSchema)], createVideoController);
+videoRouter.post(
+  "/",
+  [auth, validate(VideoSchema), upload.single("video")],
+  createVideoController
+);
 videoRouter.get("/", getVideosController);
 
 export default videoRouter;
