8dbf58ee6acaf260a9f7ce100a6a5bfdcff660f0
Matias Santillan <sntlln.93@gmail.com>
2024-04-13 03:31:36 -0300

    client/undo #b01dc9b

--- apps/client/screens/Camera/index.tsx
+++ apps/client/screens/Camera/index.tsx
@@ -126,7 +126,7 @@ export default function Camera() {
             </View>
           </View>
         </View>
-        {isRecording && <BottomBar />}
+        <BottomBar />
       </View>
     </ExpoCamera>
   );
