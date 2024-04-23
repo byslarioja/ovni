d4dc3b2da2352f6ff8d7ee0a667569fbd67d6b00
Matias Santillan <sntlln.93@gmail.com>
2024-04-23 07:12:02 -0300

    backoffice/show user id in a more friendly way

--- apps/backoffice/src/features/users-list/index.tsx
+++ apps/backoffice/src/features/users-list/index.tsx
@@ -8,6 +8,8 @@ import {
 } from "@/components/ui/table";
 import { BanUser } from "../ban-user";
 import { UsersListProps } from "@/types/props";
+import { toTitleCase } from "@/lib/utils";
+import { Link } from "@tanstack/react-router";
 
 export default function UsersList({ users }: UsersListProps) {
   return (
@@ -24,10 +26,18 @@ export default function UsersList({ users }: UsersListProps) {
       <TableBody>
         {users.map((user) => (
           <TableRow key={user.id}>
-            <TableCell className="font-medium">{user.id}</TableCell>
-            <TableCell>{user.name}</TableCell>
+            <TableCell className="font-medium">
+              <Link
+                to="/users/$userId"
+                params={{ userId: user.id }}
+                className="font-bold text-blue-800"
+              >
+                {user.id.split("-")[0]}
+              </Link>
+            </TableCell>
+            <TableCell>{toTitleCase(user.name)}</TableCell>
             <TableCell>{user.email}</TableCell>
-            <TableCell>{user.country}</TableCell>
+            <TableCell>{toTitleCase(user.country)}</TableCell>
             <TableCell>
               <BanUser userId={user.id} userEmail={user.email} />
             </TableCell>
