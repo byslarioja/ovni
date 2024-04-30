c9c204ff865c32d5c1749969e72bcd36c7bbd308
Matias Santillan <sntlln.93@gmail.com>
2024-04-30 20:38:32 -0300

    backoffice/Use TQ to fetch firebase videos because they will be cached

--- apps/backoffice/src/features/videos-list/Video.tsx
+++ apps/backoffice/src/features/videos-list/Video.tsx
@@ -6,34 +6,36 @@ import { Link } from "@tanstack/react-router";
 import { DownloadCloud, Info, User2 } from "lucide-react";
 import { handleDownloadReadings } from "./service";
 import { DeleteVideo } from "../delete-video";
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 import { Spinner } from "@/components/Spinner";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const getVideo = async (uri: string) => {
+  const response = await axios.get(uri, { responseType: "arraybuffer" });
+
+  const data = response.data;
+  return URL.createObjectURL(new Blob([data], { type: "video/mp4" }));
+};
 
 export function Video(video: VideoProps) {
-  const [isLoading, setIsLoading] = useState(true);
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.onloadeddata = () => {
-        setIsLoading(false);
-      };
-    }
-  }, []);
+  const { data, isPending } = useQuery({
+    queryKey: ["videos", video.uri],
+    queryFn: ({ queryKey: [, uri] }) => getVideo(uri),
+    staleTime: Infinity,
+  });
 
   return (
-    <Card className="min-w-[250px]">
+    <Card className="min-w-[200px]">
       <CardContent className="p-0 relative rounded-t-lg overflow-hidden">
-        {isLoading && (
+        {isPending && (
           <div className="bg-secondary absolute inset-0 flex items-center justify-center">
             <Spinner />
           </div>
         )}
-        <video
-          ref={videoRef}
-          className="object-cover h-48 w-full"
-          src={video.uri}
-        />
+        <video ref={videoRef} className="object-cover h-48 w-full" src={data} />
         <div className="absolute inset-0 w-full h-full bg-[rgba(0,0,0,0.5)] z-10"></div>
         <div className="absolute text-white right-0 top-0 py-3 px-6 z-20 text-lg text-muted-foreground">
           <p className="text-right">
--- apps/backoffice/src/features/videos-list/index.tsx
+++ apps/backoffice/src/features/videos-list/index.tsx
@@ -4,7 +4,9 @@ import { Video } from "./Video";
 export default function VideosList({ videos }: ApiVideoResponse) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {videos?.map((video) => <Video {...video} key={video.id} />)}
+      {videos.map((video) => (
+        <Video {...video} key={video.id} />
+      ))}
     </div>
   );
 }
