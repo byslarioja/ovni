550ac66f336c924ff2143a43017a3b49133ed835
Matias Santillan <sntlln.93@gmail.com>
2024-05-13 21:00:38 -0300

    client/use storage to get assets

--- apps/client/screens/Library/useVideoList.tsx
+++ apps/client/screens/Library/useVideoList.tsx
@@ -1,33 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import * as MediaLibrary from "expo-media-library";
 
-export function useAssetVideoList() {
-  const { data, isLoading } = useQuery({
-    queryKey: ["assets"],
-    queryFn: getAssetInfo,
-  });
-  return {
-    assets: data,
-    isLoading,
-  };
-}
-
-const getAssetInfo = async () => {
-  const { assets } = await MediaLibrary.getAssetsAsync({
-    mediaType: MediaLibrary.MediaType.video,
-  });
-
-  return assets;
-};
-
-// import { PersistedAsset } from "Screens/Camera/services/types";
-// import { useStorageState } from "Shared/hooks/useStorageState";
-
 // export function useAssetVideoList() {
-//   const [[isLoading, assets]] = useStorageState<PersistedAsset[]>("videos");
-
+//   const { data, isLoading } = useQuery({
+//     queryKey: ["assets"],
+//     queryFn: getAssetInfo,
+//   });
 //   return {
-//     assets,
+//     assets: data,
 //     isLoading,
 //   };
 // }
+
+// const getAssetInfo = async () => {
+//   const { assets } = await MediaLibrary.getAssetsAsync({
+//     mediaType: MediaLibrary.MediaType.video,
+//   });
+
+//   return assets;
+// };
+
+import { PersistedAsset } from "Screens/Camera/services/types";
+import { useStorageState } from "Shared/hooks/useStorageState";
+
+export function useAssetVideoList() {
+  const [[isLoading, assets]] = useStorageState<PersistedAsset[]>("videos");
+
+  return {
+    assets,
+    isLoading,
+  };
+}
